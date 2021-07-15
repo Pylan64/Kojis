a1c5b267d11ede4313a7b7681fddf5e4d8265c5a
Pylan64 <87488451+Pylan64@users.noreply.github.com>
2021-07-15 11:25:14 -0400

    Add files via upload

---
+++ pymojis.py
@@ -0,0 +1,28 @@
+#Emoji Amazing
+
+class faces():
+    def happyface():
+     return '😀'
+
+
+    def sadface():
+        return '😥'
+
+
+    def angryface():
+        return '🤬'
+
+
+    def moneyface():
+        return '🤑'
+#1.2
+    def poop():
+        return '💩'
+
+    def skull():
+        return '💀'
+
+    def ghost():
+        return '☠'
+
+
